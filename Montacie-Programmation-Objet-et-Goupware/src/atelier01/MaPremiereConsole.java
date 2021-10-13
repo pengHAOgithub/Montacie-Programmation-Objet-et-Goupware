a51235db008c300bd8184c834424d7047d655560
langueetinformatique2021 <langueetinformatique2021@gmail.com>
2021-10-13 20:25:49 +0200

    Cours 4 Programmation Objet et Groupware

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier01/MaPremiereConsole.java
@@ -0,0 +1,15 @@
+package atelier01;
+
+import util.Console;
+
+public class MaPremiereConsole {
+
+	public static void main(String[] args) {
+		
+		Console c = new Console();
+		c.println("Claude");
+		c.println("Montacié");
+		
+	}
+
+}
---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier01/simpleProgramme2.java
@@ -0,0 +1,13 @@
+package atelier01;
+
+public class simpleProgramme2 {
+
+
+	public static void main(String[] args) {
+	
+		System.out.println("Claude ");
+		System.out.println("Montacié");
+
+	}
+
+}
