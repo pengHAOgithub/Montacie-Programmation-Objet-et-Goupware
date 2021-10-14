55c1453da88eb2a4eb4a7bd084b0ba67304264e4
isha <isha@fedora>
2021-10-14 09:58:09 +0200

    modification testAB

--- Montacie-Programmation-Objet-et-Goupware/src/atelier02/testAB.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier02/testAB.java
@@ -12,9 +12,9 @@ public class testAB {
 	public static void main(String[] args) {
 
 		System.out.println(A.treize(10));
-		System.out.println(A.treize(26));
+		System.out.println(A.treize(27));
 		System.out.println(B.divise(10));
-		System.out.println(B.divise(26));
+		System.out.println(B.divise(27));
 
 	}
 
